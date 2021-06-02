a64b6fd648c9730aeabaa9430cc3bac31c9feea4
HaydenLangill <>
2021-06-02 15:02:04 +0000

    '

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ document.getElementById('color').addEventListener('click', color)
 document.getElementById('show').addEventListener('click', show)
 
 function image () {
-  document.getElementById('pizza').src = "yes.jpg" // changes image
+  document.getElementById('pizza').src = 'yes.jpg' // changes image
 }
 
 function text () {
-  document.getElementById('krab').innerHTML = "Fun fact: Did you know McDonalds served pizza!" // changes teh text
+  document.getElementById('krab').innerHTML = 'Fun fact: Did you know McDonalds served pizza!' // changes teh text
 }
 
 function hide () {
-  document.getElementById('krab').style.display = "none"
-  document.getElementById('pizza').style.display = "none" /* Makes both text and image disapper */
+  document.getElementById('krab').style.display = 'none'
+  document.getElementById('pizza').style.display = 'none' /* Makes both text and image disapper */
 }
 
 function color () {
-  document.body.style.backgroundColor = "yellow" //Changes background color
+  document.body.style.backgroundColor = 'yellow' //Changes background color 
 }
 
 function show () {
-  document.getElementById('krab').style.display = "block"
-  document.getElementById('pizza').style.display = "block" /* Makes both text and image apper */
+  document.getElementById('krab').style.display = 'block'
+  document.getElementById('pizza').style.display = 'block' /* Makes both text and image apper */
 }
