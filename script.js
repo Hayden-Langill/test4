3769e9f228a88e4925eea2ae60f0f5c299645c4d
HaydenLangill <>
2021-06-02 14:51:42 +0000

    ad

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ document.getElementById('color').addEventListener('click', color)
 document.getElementById('show').addEventListener('click', show)
 
 function image () {
-  document.getElementById('pizza').src = "yes.jpg"
+  document.getElementById('pizza').src = "yes.jpg" // changes image
 }
 
 function text () {
-  document.getElementById('Krab').innerHTML = "Fun fact: Did you know McDonalds served pizza!"
+  document.getElementById('krab').innerHTML = "Fun fact: Did you know McDonalds served pizza!" // changes teh text
 }
 
 function hide () {
-  document.getElementById('Krab').style.display = "none"
-  document.getElementById('pizza').style.display = "none"
+  document.getElementById('krab').style.display = "none"
+  document.getElementById('pizza').style.display = "none" /* Makes both text and image disapper */
 }
 
 function color () {
-  document.body.style.backgroundColor = "yellow"
+  document.body.style.backgroundColor = "yellow" //Changes background color
 }
 
 function show () {
-  document.getElementById('Krab').style.display = "block"
-  document.getElementById('pizza').style.display = "block"
+  document.getElementById('krab').style.display = "block"
+  document.getElementById('pizza').style.display = "block" /* Makes both text and image apper */
 }
